246547042832d72c3a29ca84eef671f958454d03
Anna KOSTRIKOVA <akostrik@e3r2p5.clusters.42paris.fr>
2022-11-17 12:34:03 +0100

    *

--- main.c
+++ main.c
@@ -5,12 +5,28 @@
 #include <stdlib.h>
 #include <bsd/string.h>
 #include <errno.h>
-#include "submit/libft.h"
+#include "submit/ft_printf.h"
+
+// Fichiers de rendu : Makefile, *.h, */*.h, *.c, */*.c
+// Fonctions externes autorisées : malloc free write va_start va_arg va_copy va_end
+// Libft autorisée
+// Une bibliothèque qui contient ft_printf(), imitant printf() originale
+// Contrairement à printf() originale, vous ne devez pas gérer de buffer
+// ar pour créer votre bibliothèque, libtool est interdite
+// Gérer les conversions suivantes : cspdiuxX%
+// %c Affiche un seul caractère
+// %s Affiche une chaîne de caractères (telle que définie par la convention C)
+// %p L’argument de pointeur void * doit être affiché en hexadécimal
+// %d Affiche un nombre décimal (base 10)
+// %i Affiche un entier en base 10
+// %u Affiche un nombre décimal non signé (base 10)
+// %x Affiche un nombre en hexadécimal (base 16) avec des lettres minuscules
+// %X Affiche un nombre en hexadécimal (base 16) avec des lettres majuscules
+// %% Affiche %
 
 int main(void)
 {
-	printf("\t\t\t\t\t\t\t\t\t\tlibc\tmy_func\n");
-	char c = 'a';
-	[[fallthrough]]
-	printf("isalpha(%c\t%d\t%32b\t%8x)\t%d\t%d\n",c,c,c,c,isalpha(c),ft_isalpha(c)); 
+	printf("\"%%c\" 'A' ->\t\t\t\t\t"); 
+	printf("%c\n",'A'); 
+	ft_printf("%c"); 
 }
---
+++ submit/ft_printf.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int	ft_printf(const char *str, ...)
+{
+	printf("I have got %s\n",str);
+	return (1);
+}
