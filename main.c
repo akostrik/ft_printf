10a993e6f34f282fa17d5a391009fdd90640466b
ann <akostrik@e3r2p6.clusters.42paris.fr>
2022-11-30 13:16:55 +0100

    final

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: akostrik <akostrik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 13:57:16 by akostrik          #+#    #+#             */
-/*   Updated: 2022/11/30 12:31:21 by akostrik         ###   ########.fr       */
+/*   Updated: 2022/11/30 13:06:17 by akostrik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,6 +40,55 @@ int main(void)
 	a = ft_printf("char\t\t [%c]",c);
 	printf("\t\t\t\treturns %zu\n\n",a);
 
+	c = '0';
+	a =   printf("char\t\t [ %c ]",c);
+	printf("\t\t\t\treturns %zu\n",a);
+	a = ft_printf("char\t\t [ %c ]",c);
+	printf("\t\t\t\treturns %zu\n\n",a);
+
+	c = '0' - 256;
+	a =   printf("char\t\t [ %c ]",c);
+	printf("\t\t\t\treturns %zu\n",a);
+	a = ft_printf("char\t\t [ %c ]",c);
+	printf("\t\t\t\treturns %zu\n\n",a);
+
+	c = '0' + 256;
+	a =   printf("char\t\t [ %c ]",c);
+	printf("\t\t\t\treturns %zu\n",a);
+	a = ft_printf("char\t\t [ %c ]",c);
+	printf("\t\t\t\treturns %zu\n\n",a);
+
+	char c1 ='0', c2 = 0, c3 = '1';
+	a =   printf("char\t\t %c %c %c ",c);
+	printf("\t\t\t\treturns %zu\n",a);
+	a = ft_printf("char\t\t %c %c %c ",c);
+	printf("\t\t\t\treturns %zu\n\n",a);
+
+	c1 =' '; c2 = ' '; c3 = ' ';
+	a =   printf("char\t\t %c %c %c ",c);
+	printf("\t\t\t\treturns %zu\n",a);
+	a = ft_printf("char\t\t %c %c %c ",c);
+	printf("\t\t\t\treturns %zu\n\n",a);
+
+	c1 ='1'; c2 = '2'; c3 = '3';
+	a =   printf("char\t\t %c %c %c ",c);
+	printf("\t\t\t\treturns %zu\n",a);
+	a = ft_printf("char\t\t %c %c %c ",c);
+	printf("\t\t\t\treturns %zu\n\n",a);
+
+	c1 ='2'; c2 = '1'; c3 = 0;
+	a =   printf("char\t\t %c %c %c ",c);
+	printf("\t\t\t\treturns %zu\n",a);
+	a = ft_printf("char\t\t %c %c %c ",c);
+	printf("\t\t\t\treturns %zu\n\n",a);
+
+	c1 = 0; c2 = '1'; c3 = '2';
+	a =   printf("char\t\t %c %c %c ",c);
+	printf("\t\t\t\treturns %zu\n",a);
+	a = ft_printf("char\t\t %c %c %c ",c);
+	printf("\t\t\t\treturns %zu\n\n",a);
+
+	/////////////////////////////////////////////
 	void *p = (void *)(&c);
 	a =   printf("pointer\t\t %p",p);
 	printf("\t\t\treturns %zu\n",a);
@@ -51,6 +100,79 @@ int main(void)
 	printf("\t\t\t\treturns %zu\n",a);
 	a = ft_printf("pointer\t\t %p",p);
 	printf("\t\t\t\treturns %zu\n\n",a);
+
+	p = -1;
+	a =   printf("pointer\t\t %p",p);
+	printf("\t\treturns %zu\n",a);
+	a = ft_printf("pointer\t\t %p",p);
+	printf("\t\treturns %zu\n\n",a);
+
+	p = 1;
+	a =   printf("pointer\t\t %p",p);
+	printf("\t\t\t\treturns %zu\n",a);
+	a = ft_printf("pointer\t\t %p",p);
+	printf("\t\t\t\treturns %zu\n\n",a);
+
+	p = 15;
+	a =   printf("pointer\t\t %p",p);
+	printf("\t\t\t\treturns %zu\n",a);
+	a = ft_printf("pointer\t\t %p",p);
+	printf("\t\t\t\treturns %zu\n\n",a);
+
+	p = 16;
+	a =   printf("pointer\t\t %p",p);
+	printf("\t\t\t\treturns %zu\n",a);
+	a = ft_printf("pointer\t\t %p",p);
+	printf("\t\t\t\treturns %zu\n\n",a);
+
+	p = 17;
+	a =   printf("pointer\t\t %p",p);
+	printf("\t\t\t\treturns %zu\n",a);
+	a = ft_printf("pointer\t\t %p",p);
+	printf("\t\t\t\treturns %zu\n\n",a);
+
+	p = LONG_MIN;
+	a =   printf("pointer\t\t %p",p);
+	printf("\t\treturns %zu\n",a);
+	a = ft_printf("pointer\t\t %p",p);
+	printf("\t\treturns %zu\n\n",a);
+
+	p = LONG_MAX;
+	a =   printf("pointer\t\t %p",p);
+	printf("\t\treturns %zu\n",a);
+	a = ft_printf("pointer\t\t %p",p);
+	printf("\t\treturns %zu\n\n",a);
+
+	p = INT_MIN;
+	a =   printf("pointer\t\t %p",p);
+	printf("\t\treturns %zu\n",a);
+	a = ft_printf("pointer\t\t %p",p);
+	printf("\t\treturns %zu\n\n",a);
+
+	p = INT_MAX;
+	a =   printf("pointer\t\t %p",p);
+	printf("\t\t\treturns %zu\n",a);
+	a = ft_printf("pointer\t\t %p",p);
+	printf("\t\t\treturns %zu\n\n",a);
+
+	p = ULONG_MAX;
+	a =   printf("pointer\t\t %p",p);
+	printf("\t\treturns %zu\n",a);
+	a = ft_printf("pointer\t\t %p",p);
+	printf("\t\treturns %zu\n\n",a);
+
+	p = -ULONG_MAX;
+	a =   printf("pointer\t\t %p",p);
+	printf("\t\t\t\treturns %zu\n",a);
+	a = ft_printf("pointer\t\t %p",p);
+	printf("\t\t\t\treturns %zu\n\n",a);
+
+	void *p1 = 0;
+	void *p2 = 0;
+	a =   printf("pointer\t\t %p %p",p1,p2);
+	printf("\t\t\treturns %zu\n",a);
+	a = ft_printf("pointer\t\t %p %p",p1,p2);
+	printf("\t\t\treturns %zu\n\n",a);
 
 	// decimal number  16 bit %d INT_MIN ... INT_MAX
 	// integer base 10 16 bit %i INT_MIN ... INT_MAX
@@ -104,6 +226,7 @@ int main(void)
 	a = ft_printf("decimal\t\t %%d = %d",n);
 	printf("\t\treturns %zu\n\n",a);
 
+	n = INT_MAX + 1;
 	a =   printf("int\t\t %%i = %i",n);
 	printf("\t\treturns %zu\n",a);
 	a = ft_printf("int\t\t %%i = %i",n);
@@ -115,6 +238,7 @@ int main(void)
 	a = ft_printf("decimal\t\t %%d = %d",n);
 	printf("\t\treturns %zu\n\n",a);
 
+	n = INT_MIN;
 	a =   printf("int\t\t %%i = %i",n);
 	printf("\t\treturns %zu\n",a);
 	a = ft_printf("int\t\t %%i = %i",n);
@@ -126,10 +250,36 @@ int main(void)
 	a = ft_printf("decimal\t\t %%d = %d",n);
 	printf("\t\treturns %zu\n\n",a);
 	
+	n = LONG_MAX;
 	a =   printf("int\t\t %%i = %i",n);
-	printf("\t\treturns %zu\n",a);
+	printf("\t\t\treturns %zu\t// LONG_MAX\n",a);
 	a = ft_printf("int\t\t %%i = %i",n);
-	printf("\t\treturns %zu\n\n",a);
+	printf("\t\t\treturns %zu\n\n",a);
+
+	n = LONG_MIN;
+	a =   printf("int\t\t %%i = %i",n);
+	printf("\t\t\t\treturns %zu\t// LONG_MIN\n",a);
+	a = ft_printf("int\t\t %%i = %i",n);
+	printf("\t\t\t\treturns %zu\n\n",a);
+
+	n = UINT_MAX;
+	a =   printf("int\t\t %%i = %i",n);
+	printf("\t\t\treturns %zu\t// UINT_MAX\n",a);
+	a = ft_printf("int\t\t %%i = %i",n);
+	printf("\t\t\treturns %zu\n\n",a);
+
+	n = ULONG_MAX;
+	a =   printf("int\t\t %%i = %i",n);
+	printf("\t\t\treturns %zu\t// ULONG_MAX\n",a);
+	a = ft_printf("int\t\t %%i = %i",n);
+	printf("\t\t\treturns %zu\n\n",a);
+
+	n = 9223372036854775807LL;
+	a =   printf("int\t\t %%i = %i",n);
+	printf("\t\t\treturns %zu\t// 9223372036854775807LL\n",a);
+	a = ft_printf("int\t\t %%i = %i",n);
+	printf("\t\t\treturns %zu\n\n",a);
+
 
 // unsigned decimal number 16 bit %u 0 ... UINT_MAX
 	unsigned int	un = 12;
@@ -140,9 +290,9 @@ int main(void)
 
 	un = 12;
 	a =    printf("int              %%i = %i",n);
-	printf("\t\treturns %zu\t // 12\n",a);
+	printf("\t\t\treturns %zu\t // 12\n",a);
 	a = ft_printf("int              %%i = %i",n);
-	printf("\t\treturns %zu\n\n",a);
+	printf("\t\t\treturns %zu\n\n",a);
 
 	un = 0;
 	a =   printf("unsigned decimal %%u = %u",un);
@@ -152,9 +302,9 @@ int main(void)
 
 	un = 0;
 	a =   printf("int              %%i = %i",n);
-	printf("\t\treturns %zu\t // 0\n",a);
+	printf("\t\t\treturns %zu\t // 0\n",a);
 	a = ft_printf("int              %%i = %i",n);
-	printf("\t\treturns %zu\n\n",a);
+	printf("\t\t\treturns %zu\n\n",a);
 
 	un = INT_MAX;
 	a =   printf("unsigned decimal %%u = %u",un);
@@ -164,9 +314,9 @@ int main(void)
 
 	un = INT_MAX;
 	a =   printf("int              %%i = %i",n);
-	printf("\t\treturns %zu\t// INT_MAX\n",a);
+	printf("\t\t\treturns %zu\t// INT_MAX\n",a);
 	a = ft_printf("int              %%i = %i",n);
-	printf("\t\treturns %zu\n\n",a);
+	printf("\t\t\treturns %zu\n\n",a);
 
 	un = -1;
 	a =   printf("unsigned decimal %%u = %u",un);
@@ -176,9 +326,9 @@ int main(void)
 
 	un = -1;
 	a =   printf("int              %%i = %i",n);
-	printf("\t\treturns %zu\t// -1\n",a);
+	printf("\t\t\treturns %zu\t// -1\n",a);
 	a = ft_printf("int              %%i = %i",n);
-	printf("\t\treturns %zu\n\n",a);
+	printf("\t\t\treturns %zu\n\n",a);
 
 	un = UINT_MAX;
 	a =   printf("unsigned decimal %%u = %u",un);
@@ -188,9 +338,9 @@ int main(void)
 
 	un = UINT_MAX;
 	a =   printf("int              %%i = %i",n);
-	printf("\t\treturns %zu\t// UINT_MAX\n",a);
+	printf("\t\t\treturns %zu\t// UINT_MAX\n",a);
 	a = ft_printf("int              %%i = %i",n);
-	printf("\t\treturns %zu\n\n",a);
+	printf("\t\t\treturns %zu\n\n",a);
 
 	un = UINT_MAX + 1;
 	a =    printf("unsigned decimal %%u = %u",un);
@@ -200,9 +350,9 @@ int main(void)
 
 	un = UINT_MAX + 1;
 	a =    printf("int              %%i = %i",n);
-	printf("\t\treturns %zu\t// UINT_MAX + 1\n",a);
+	printf("\t\t\treturns %zu\t// UINT_MAX + 1\n",a);
 	a = ft_printf("int              %%i = %i",n);
-	printf("\t\treturns %zu\n\n",a);
+	printf("\t\t\treturns %zu\n\n",a);
 
 	un = UINT_MAX + 2;
 	a =    printf("unsigned decimal %%u = %u",un);
@@ -212,9 +362,9 @@ int main(void)
 
 	un = UINT_MAX + 2;
 	a =    printf("int              %%i = %i",n);
-	printf("\t\treturns %zu\t// UINT_MAX + 2\n",a);
+	printf("\t\t\treturns %zu\t// UINT_MAX + 2\n",a);
 	a = ft_printf("int              %%i = %i",n);
-	printf("\t\treturns %zu\n\n",a);
+	printf("\t\t\treturns %zu\n\n",a);
 
 // unsigned int %u x 0 ... UINT_MAX
 	unsigned int	x = 12;
